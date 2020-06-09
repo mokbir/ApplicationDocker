4d0698a3ac545149ea7d887a73354aec1ed2ecc0
El-Kabir Moutuou <ekmoutuou@larochellegc.com>
2020-06-08 20:51:59 -0400

    Add project files.

---
+++ ApplicationDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApplicationDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
